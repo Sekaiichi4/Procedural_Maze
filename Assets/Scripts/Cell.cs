27a05e5e4b10e3ab077f7133decec6fee1375e2e
NakataNoGyaku <mert1996@live.nl>
2020-03-27 17:35:27 +0100

    Adds grid visualization.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public int xPos, yPos;
+    /// <summary>
+    /// Top, Right, Bottom, Left
+    /// </summary>
+    public bool[] walls = { true, true, true, true };
+    public bool visited = false;
+
+    void Start()
+    {
+
+    }
+
+    public void InitCell(int _x, int _y, float _gridSize)
+    {
+        xPos = _x;
+        yPos = _y;
+
+        //We want to initialize the new location of the cell in the grid from the top left till the bottom right.
+        transform.localPosition = new Vector3(_x * _gridSize, (-_y) * _gridSize, gameObject.transform.position.z);
+    }
+
+    public void VisitCell()
+    {
+        visited = true;
+
+        Debug.Log("Visited cell with position: " + xPos + "," + yPos);
+
+        //TODO: Change this to a method that decides which sprite to assign to it.
+        GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Maze/1111v");
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MazeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    public int gridWidth, gridHeight;
+    public GameObject cellObject;
+
+    private float cellSize = 2.56f;
+    private GameObject[,] grid;
+    private Cell currentCell;
+
+    void Start()
+    {
+        //Transform the parent grid gameobject so that the cells are centered in the screen. 
+        transform.position = new Vector3((-gridWidth) * (cellSize / 2) + (cellSize / 2), (gridHeight) * (cellSize / 2) - (cellSize / 2), gameObject.transform.position.z);
+
+        //Create the basic grid for the maze.
+        CreateGrid();
+
+        //Start at the first cell.
+        grid[0, 0].GetComponent<Cell>().VisitCell();
+    }
+
+    void CreateGrid()
+    {
+        grid = new GameObject[gridWidth, gridHeight];
+
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                GameObject mCell = grid[x, y] = Instantiate(cellObject, gameObject.transform);
+
+                mCell.GetComponent<Cell>().InitCell(x, y, cellSize);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
